2449aac00311af7d81d1bd9504a368c3c196828a
lyz1202dt <cloud1202@qq.com>
2026-03-30 19:00:57 +0800

    增加真实机械臂launch文件中link4到相机link的静态变换的发布

--- src/launch_pack/launch/arm_real.launch.py
+++ src/launch_pack/launch/arm_real.launch.py
@@ -39,9 +39,22 @@ def generate_launch_description():
         arguments=["-d", rviz_path],
     )
 
+    static_tf_camera = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=[
+            "0.1", "0.09", "-0.03",  # x, y, z translation
+            "0.0", "0.7071068", "0.0", "0.7071068",  # quaternion (x, y, z, w) - 90° rotation about Y
+            "link4",
+            "camera_link"
+        ],
+        output="screen",
+    )
+
     return LaunchDescription([
         arm_driver,
         robot_state_pub,
         arm_calc,
         rviz2,
+        static_tf_camera
     ])
